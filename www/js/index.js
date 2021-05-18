2438f847172ed3c7eed9869bf888111e7281425b
messiasGeovani <messias.geovani.lima@gmail.com>
2021-05-17 21:13:27 -0300

    first journey complete

--- www/js/index.js
+++ www/js/index.js
@@ -1,14 +1,18 @@
 $(document).ready(() => {
   const isAlreadyOpened = localStorage.getItem("@App:already_opened");
+  const isUnfinishedRegistration = localStorage.getItem(
+    "@App:registration_unfinished"
+  );
   const userData = localStorage.getItem("@App:auth_data");
 
-  window.location.replace("pages/register.html");
+  if (isAlreadyOpened) {
+    if (isUnfinishedRegistration)
+      return window.location.replace("pages/signup.html");
 
-  // if (isAlreadyOpened) {
-  //   window.location.replace(
-  //     userData && userData.authToken ? "pages/main.html" : "pages/login.html"
-  //   );
-  // } else {
-  //   window.location.replace("pages/welcome.html");
-  // }
+    window.location.replace(
+      userData && userData.authToken ? "pages/main.html" : "pages/login.html"
+    );
+  } else {
+    window.location.replace("pages/welcome.html");
+  }
 });
--- www/js/register.js
+++ www/js/register.js
@@ -1,11 +1,72 @@
 $(document).ready(() => {
+  localStorage.setItem("@App:registration_unfinished", true);
+
   $("#disease_container").hide();
+  $("#pre_disease_container").hide();
+  $("#diet_container").hide();
+
+  for (let index = 2; index <= 5; index++) {
+    $(`#section${index}`).hide();
+  }
 
   $("#disease").change(function () {
     if (this.checked) {
       $("#disease_container").show();
     } else {
       $("#disease_container").hide();
+    }
+  });
+  $("#no_disease").change(() => $("#disease_container").hide());
+
+  $("#pre_disease").change(function () {
+    if (this.checked) {
+      $("#pre_disease_container").show();
+    } else {
+      $("#pre_disease_container").hide();
+    }
+  });
+  $("#no_pre_disease").change(() => $("#pre_disease_container").hide());
+
+  $("#has_diet").change(function () {
+    if (this.checked) {
+      $("#diet_container").show();
+    } else {
+      $("#diet_container").hide();
+    }
+  });
+  $("#has_no_diet").change(() => $("#diet_container").hide());
+
+  $("[type='text']").focusin(() => $("#action_button").hide());
+  $("[type='text']").focusout(() => $("#action_button").show());
+
+  let currentSection = 1;
+  $("#action_button").click(() => {
+    for (let index = currentSection; index <= 5; index++) {
+      if (index === 5) {
+        $("#action_button")
+          .removeClass(["cyan", "lighten-2"])
+          .addClass(["red", "darken-1"])
+          .text("Finalizar");
+      }
+      if ($(`#section${index + 1}`).is(":hidden")) {
+        $(`#section${index}`).hide();
+        $(`#section${index + 1}`).show();
+        currentSection++;
+        break;
+      }
+    }
+  });
+  $("#back_button").click(() => {
+    for (let index = currentSection; index >= 1; index--) {
+      if (index === 1) {
+        window.location.replace("../index.html");
+      }
+      if ($(`#section${index - 1}`).is(":hidden")) {
+        $(`#section${index}`).hide();
+        $(`#section${index - 1}`).show();
+        currentSection--;
+        break;
+      }
     }
   });
 });
